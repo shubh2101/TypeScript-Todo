02227754e1c4036c6d80177ab49c4bda21c34b21
Shubham Singh <shubham2101@gmail.com>
2023-10-11 21:03:08 +0530

    storing tasks to local storage

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
+const tasks: Task[] = loadTasks();
+tasks.forEach(addNewtask);
+
 form?.addEventListener("submit", (event) => {
   event.preventDefault();
   if (input?.value == "" || input?.value == null) return;
@@ -21,7 +24,10 @@ form?.addEventListener("submit", (event) => {
     completed: false,
     createdAt: new Date(),
   };
+  tasks.push(newTask);
   addNewtask(newTask);
+  saveTasks();
+  input.value = "";
 });
 
 function addNewtask(task: Task) {
@@ -29,8 +35,22 @@ function addNewtask(task: Task) {
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
   checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const tasksJson = localStorage.getItem("TASKS");
+  if (tasksJson == null) return [];
+  return JSON.parse(tasksJson);
 }
